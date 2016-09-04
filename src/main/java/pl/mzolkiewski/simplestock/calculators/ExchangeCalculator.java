29ff325ad1b914b31a11d1481113c890725412ed
mzolkiewski <zolkiewski.marcin@gmail.com>
2016-09-04 12:47:50 +0200

    implemented exchange calculator

--- src/main/java/pl/mzolkiewski/simplestock/calculators/ExchangeCalculator.java
+++ src/main/java/pl/mzolkiewski/simplestock/calculators/ExchangeCalculator.java
@@ -1,25 +1,48 @@
 package pl.mzolkiewski.simplestock.calculators;
 
 import com.google.inject.Inject;
+import com.google.inject.Singleton;
 import java.util.ArrayList;
 import java.util.List;
 import pl.mzolkiewski.simplestock.exchanges.Exchange;
+import pl.mzolkiewski.simplestock.exchanges.IExchangeService;
+import pl.mzolkiewski.simplestock.stocks.AbstractStock;
+import pl.mzolkiewski.simplestock.stocks.IStockService;
 
 /**
  *
  * @author Martin
  */
+@Singleton
 public class ExchangeCalculator implements IExchangeCalculator {
+    private IStockService stockService;
     private IStockCalculator stockCalculator;
     
     @Inject
-    public ExchangeCalculator(IStockCalculator stockCalculator) {
+    public ExchangeCalculator(IStockService stockService, IStockCalculator stockCalculator) {
+        this.stockService = stockService;
         this.stockCalculator = stockCalculator;
     }
 
     @Override
     public double allShareIndex(String exchangeSymbol) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        List<AbstractStock> stocks = getStocks(exchangeSymbol);
+        
+        if (stocks.isEmpty()) {
+            return 0;
+        }
+        
+        double[] prices = new double[stocks.size()];
+        
+        for (int i = 0; i < stocks.size(); i++) {
+            prices[i] = stockCalculator.price(stocks.get(i).getStockSymbol());
+        }
+        
+        return Calculator.geometricMean(prices);
+    }
+    
+    private List<AbstractStock> getStocks(String exchangeSymbol) {
+        return stockService.getByExchangeSymbol(exchangeSymbol);
     }
     
 }
---
+++ src/test/java/pl/mzolkiewski/simplestock/calculators/ExchangeCalculatorTest.java
@@ -0,0 +1,61 @@
+package pl.mzolkiewski.simplestock.calculators;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.mockito.Matchers;
+import org.mockito.Mockito;
+import pl.mzolkiewski.simplestock.stocks.AbstractStock;
+import pl.mzolkiewski.simplestock.stocks.CommonStock;
+import pl.mzolkiewski.simplestock.stocks.StockService;
+
+/**
+ *
+ * @author Martin
+ */
+public class ExchangeCalculatorTest {
+    private StockService mockStockService;
+    private StockCalculator mockStockCalculator;
+    private ExchangeCalculator calculator;
+    
+    @Before
+    public void setUp() {
+        mockStockService = Mockito.mock(StockService.class);
+        mockStockCalculator = Mockito.mock(StockCalculator.class);
+        calculator = new ExchangeCalculator(mockStockService, mockStockCalculator);
+    }
+    
+    @After
+    public void tearDown() {
+        mockStockService = null;
+        mockStockCalculator = null;
+        calculator = null;
+    }
+
+    @Test
+    public void testAllShareIndex() {
+        String exchangeSymbol = "MAO";
+        List<AbstractStock> stocks = new ArrayList<>();
+        stocks.add(new CommonStock("LOL", exchangeSymbol, 1, 1));
+        stocks.add(new CommonStock("xD", exchangeSymbol, 1, 1));
+        
+        Mockito.when(mockStockService.getByExchangeSymbol(exchangeSymbol)).thenReturn(stocks);
+        Mockito.when(mockStockCalculator.price(Matchers.anyString())).thenReturn(2.0, 8.0);
+        
+        double allShareIndex = calculator.allShareIndex(exchangeSymbol);
+        
+        assertEquals(4.0, allShareIndex, 0);
+    }
+    
+    @Test
+    public void testAllShareIndex_noStocks() {
+        double allShareIndex = calculator.allShareIndex("MAO");
+        
+        assertEquals(0, allShareIndex, 0);
+    }
+}
